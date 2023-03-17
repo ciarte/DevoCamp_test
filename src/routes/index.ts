d281746526c9b1987220ffa6ac7052919e3c42af
Rotela Domingo Ramon <74286462+rotela5@users.noreply.github.com>
2023-03-17 05:48:47 -0300

    rutas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { router as routerPostulaciones } from "./postulaciones";
+import { router as routerEmpresas } from "./empresas";
 
 const router = Router();
 
 router.get("/postulaciones", routerPostulaciones);
+router.get("/empresas", routerEmpresas);
 
 export default router;
--- src/routes/postulaciones.ts
+++ src/routes/postulaciones.ts
@@ -1,12 +1,66 @@
-import { Router } from "express";
 
-export const router = Router();
+import  express  from "express";
 
-router.get("/postulaciones", (_req, res) => {
-  res.send("test ruta postulaciones");
+export const router = express.Router();
+
+//  Model
+const Postulaciones = require('../models/Postulantes');
+
+// GET all 
+router.get('/', async (_req , res) => {
+  const postulacione = await Postulaciones.find();
+  res.json(postulacione);
 });
-router.post("/postulaciones", (_req, res) => {
-  //let bodyData = req.body;
 
-  res.status(201).json({ status: "ok" });
+// GET all 
+router.get('/:name', async (req, res) => {
+  try{
+    const postulacione = await Postulaciones.findByName(req.params.name);
+    res.json(postulacione);
+  }catch(error) {
+    res.status(500).json(error)
+    return;
+  }
+  
 });
+
+// ADD a new 
+router.post('/', async (req, res) => {
+  try{
+    const {name,email,linkedin,porfolio,presentationLetter,cv } = req.body;
+    const postulacione = new Postulaciones({name,email,linkedin,porfolio,presentationLetter,cv});
+    await postulacione.save();
+    res.json({status: 'Postulaciones Saved'});
+  }catch(error) {
+    res.status(500).json(error)
+    return;
+  }
+  
+});
+
+// UPDATE a new 
+router.put('/:name', async (req, res) => {
+  try{
+    const { name,email,linkedin,porfolio,presentationLetter,cv } = req.body;
+    const newPostulaciones = {name,email,linkedin,porfolio,presentationLetter,cv};
+    await Postulaciones.findByNameAndUpdate(req.params.name, newPostulaciones);
+    res.json({status: 'Postulaciones Updated'});
+  }catch(error) {
+    res.status(500).json(error)
+    return;
+  }
+  
+});
+router.delete('/:name', async (req, res) => {
+  try{
+    await Postulaciones.findByNameAndRemove(req.params.name);
+    res.json({status: 'Postulaciones Deleted'});
+  }catch(error) {
+    res.status(500).json(error)
+    return;
+  }
+  
+});
+
+
+
