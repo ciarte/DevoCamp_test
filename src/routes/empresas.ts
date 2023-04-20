36933c356789b8458bb4274cda5d4caf270cb31d
danielDeVita <danielitodevita@gmail.com>
2023-04-20 17:48:48 -0300

    modified deleteUploadContents

--- src/routes/empresas.ts
+++ src/routes/empresas.ts
@@ -1,7 +1,7 @@
 import { Router, Response, Request } from "express";
 import { Empresas } from '../models/Empresas';
 import upload from '../middlewares/empresasMulter'; /* MANAGES THE FILE UPLOAD */
-import deleteUploadsContents from '../utils/deleteUploadsContents'; /* DELETES FILES IN UPLOADS FOLDER, NO THE FOLDER ITSELF */
+import deleteUploadsContents from '../utils/deleteUploadsContents'; /* DELETES UPLOADING FILE IN UPLOADS FOLDER, NO THE FOLDER ITSELF NOR THE PREVIOUS FILES */
 import validateBody from '../middlewares/empresasValidator'; /* EXPRESS VALIDATOR FOR /EMPRESAS POST REQUEST */
 import { validationResult } from 'express-validator';
 
@@ -56,19 +56,17 @@ router.post("/empresas",async (req, res) => {
 /* ROUTE TO UPLOAD COMPANY INFORMATION */
 router.post("/", upload.single("archivoAdjunto"), validateBody, async (req: Request, res: Response) => {
   try {
-    if (!validationResult(req).isEmpty()) {
-      deleteUploadsContents();
-      return res.status(400).json({ errors: validationResult(req).array() });
-    }
     const { interes, nombre, correo, tel, presentacion } = req.body;
     const archivoAdjunto = req.file;
+    if (!validationResult(req).isEmpty()) {
+      deleteUploadsContents(archivoAdjunto?.filename);
+      return res.status(400).json({ errors: validationResult(req).array() });
+    }
     if (!interes || !nombre || !correo || !tel || !presentacion || !archivoAdjunto) throw new Error("Faltan completar campos");
     const newEmpresa = new Empresas({ interes, nombre, correo, tel, presentacion, archivoAdjunto: archivoAdjunto.path, });
     const savedEmpresa = await newEmpresa.save();
-    deleteUploadsContents();
     return res.status(201).json({ savedEmpresa, message: "Hemos recibido su solicitud" });
   } catch (error: any) {
-    deleteUploadsContents();
     return res.status(500).send(error.message);
   }
 });
--- src/utils/deleteUploadsContents.ts
+++ src/utils/deleteUploadsContents.ts
@@ -1,13 +1,10 @@
 import fs from "fs";
 import path from "path";
 
-const deleteUploadsContents = () => {
-    if (fs.existsSync(path.resolve(__dirname, "../uploads"))) {
-        const files = fs.readdirSync(path.resolve(__dirname, "../uploads"));
-        for (const file of files) {
-            fs.unlinkSync(path.resolve(__dirname, `../uploads/${file}`));
-        }
+const deleteUploadsContents = (filename?: string): void => {
+    if (filename && fs.existsSync(path.resolve(__dirname, "../uploads", filename))) {
+        fs.unlinkSync(path.resolve(__dirname, "../uploads", filename));
     }
-}
+};
 
 export default deleteUploadsContents
