d6ab1474103a339a6e867936b25ddabf8c29fdf6
danielDeVita <danielitodevita@gmail.com>
2023-04-27 13:35:49 -0300

    correctingNodemailer for /empresas POST

--- src/routes/empresas.ts
+++ src/routes/empresas.ts
@@ -4,8 +4,6 @@ import upload from '../middlewares/empresasMulter'; /* MANAGES THE FILE UPLOAD *
 import deleteUploadsContents from '../utils/deleteUploadsContents'; /* DELETES UPLOADING FILE IN UPLOADS FOLDER, NO THE FOLDER ITSELF NOR THE PREVIOUS FILES */
 import validateBody from '../middlewares/empresasValidator'; /* EXPRESS VALIDATOR FOR /EMPRESAS POST REQUEST */
 import { validationResult } from 'express-validator';
-
-
 import EmailController from "../controllers/Email";
 import createEmailApplicants from "../utils/conts";
 
@@ -17,55 +15,56 @@ router.get("/empresas", (_req, res) => {
   res.send("test ruta");
 });
 
-router.post("/empresas",async (req, res) => {
-  //let bodyData = req.body;
-   try{
-    const email = EmailController;
-    const emailRequest = req;
-
-    emailRequest.body = {
-      to: req.body.email,
+/* router.post("/empresas", async (req, res) => {
+  try {
+    req.body = {
+      to: req.body.Email,
       subject: "correo enviado",
       message: createEmailApplicants(req.body.Email),
     };
-        
-   email.send(emailRequest, res);
+    await EmailController.send(req, res);
     return res.status(201).json({
       status: "Ok",
       result: "email enviado con éxito",
     });
-
-   }catch (error) {
-    res.status(500).json(error);
-    return;
+  } catch (error) {
+    return res.status(500).json(error);
   }
-     
-})
-
- 
- 
-
- 
- 
-  
-
-  
-
-  
+}) */
 
 /* ROUTE TO UPLOAD COMPANY INFORMATION */
 router.post("/", upload.single("archivoAdjunto"), validateBody, async (req: Request, res: Response) => {
   try {
     const { interes, nombre, correo, tel, presentacion } = req.body;
     const archivoAdjunto = req.file;
+
+    /* EXPRESS-VALIDATOR SECTIO */
     if (!validationResult(req).isEmpty()) {
       deleteUploadsContents(archivoAdjunto?.filename);
       return res.status(400).json({ errors: validationResult(req).array() });
     }
+    /*  */
+
+    /* MONGOOSE SECTION */
     if (!interes || !nombre || !correo || !tel || !presentacion || !archivoAdjunto) throw new Error("Faltan completar campos");
     const newEmpresa = new Empresas({ interes, nombre, correo, tel, presentacion, archivoAdjunto: archivoAdjunto.path, });
     const savedEmpresa = await newEmpresa.save();
-    return res.status(201).json({ savedEmpresa, message: "Hemos recibido su solicitud" });
+    /*  */
+
+    /* NODEMAILER SECTION */
+    req.body = {
+      to: correo,
+      subject: "correo enviado",
+      message: createEmailApplicants(correo),
+    };
+    await EmailController.send(req, res);
+    /*  */
+
+    return res.status(201).json({
+      savedEmpresa, message: "Hemos recibido su solicitud",
+      status: "Ok",
+      result: "E-mail enviado con éxito",
+    });
   } catch (error: any) {
     return res.status(500).send(error.message);
   }
