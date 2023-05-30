e28313c0a372b247a22984148eae3435ac855e10
Gabriel Iciarte <gabriel_viki@hotmail.com>
2023-05-30 20:28:04 -0300

    pm2 test

--- src/config/mongoDB.ts
+++ src/config/mongoDB.ts
@@ -1,22 +1,22 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-// export const mongoConnect = async () => {
-//   const DB_URI_LOCAL = <string>process.env.DB_URI_LOCAL;
-//   try {
-//     await mongoose.connect(DB_URI_LOCAL);
-//     console.log("DB connected");
-//   } catch (error) {
-//     console.error("Failed to connect to MongoDB\n", error);
-//   }
-// };
-
 export const mongoConnect = async () => {
-  const DB_URI_CLOUD = <string>process.env.DB_URI_CLOUD;
+  const DB_URI_LOCAL = <string>process.env.DB_URI_LOCAL;
   try {
-    await mongoose.connect(DB_URI_CLOUD);
+    await mongoose.connect(DB_URI_LOCAL);
     console.log("DB connected");
   } catch (error) {
     console.error("Failed to connect to MongoDB\n", error);
   }
 };
+
+// export const mongoConnect = async () => {
+//   const DB_URI_CLOUD = <string>process.env.DB_URI_CLOUD;
+//   try {
+//     await mongoose.connect(DB_URI_CLOUD);
+//     console.log("DB connected");
+//   } catch (error) {
+//     console.error("Failed to connect to MongoDB\n", error);
+//   }
+// };
