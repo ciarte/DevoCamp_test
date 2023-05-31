9bd676ad5c06c775aefa35a24393e1a2f1e2754f
Gabriel Iciarte <gabriel_viki@hotmail.com>
2023-05-31 19:53:41 -0300

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: '.build/server.js',
+    watch: '.'
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'https://github.com/ciarte/DevoCamp_test',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
